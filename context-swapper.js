36cb8191da24c7c37a4727697b54a24ec9b45e61
Tim Vergenz <vergenzt@gmail.com>
2015-12-02 08:48:40 -0500

    Get port from environment variable

--- context-swapper.js
+++ context-swapper.js
@@ -10,7 +10,8 @@ app.get('/', function(req, res) {
   res.send('Test!');
 });
 
-app.listen(8080, function() {
-  console.log('Listening on port 8080.');
+var port = process.env['PORT'] || 8080;
+app.listen(port, function() {
+  console.log('Listening on port ' + port + '.');
 });
 
