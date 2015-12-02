ae4ece963610146228f2cf0272388f6586e60aa1
Tim Vergenz <vergenzt@gmail.com>
2015-12-02 09:36:00 -0500

    Add basic tag fetching

--- context-swapper.js
+++ context-swapper.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 
-var rtm = require('rtm-js')(
+var rtm = new (require('rtm-js'))(
     process.env['RTM_API_KEY'],
-    process.env['RTM_API_SECRET']
+    process.env['RTM_API_SECRET'],
+    'delete'
 );
+rtm.auth_token = process.env['RTM_AUTH_TOKEN'];
 
-app.get('/', function(req, res) {
-  res.send('Test!');
+app.put('/:tag', function(req, res) {
+  var tag = req.params['tag'];
+  rtm.get('rtm.tasks.getList', {filter: 'tag:' + tag}, function(rsp) {
+    res.send(rsp);
+  });
 });
 
 var port = process.env['PORT'] || 8080;
